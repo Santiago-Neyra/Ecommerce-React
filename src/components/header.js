0ffea3913e724231e22dc27677fe721b7d5d09c7
Santiago Neyra <santineyra97@gmail.com>
2022-02-03 16:51:34 -0300

    a

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+function Header() {
+    
+    return( <div className="header">
+        <h1 className="titulo">Dual Key gaming</h1>
+        <ul className="lista">
+        <li className="items"> <a href="#">componentes</a></li>
+        <li className="items"><a  href="#">Pc completa</a></li>
+        <li className="items"><a  href="#"> Perifericos</a></li>
+        </ul>
+     </div>
+        
+        
+
+
+
+    )
+}
+export default Header;
