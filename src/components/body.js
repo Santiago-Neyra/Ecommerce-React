bb6a99e78ae3e231a5684390dafe3ecc4355ced8
Santiago Neyra <santineyra97@gmail.com>
2022-02-04 21:17:47 -0300

    a

---
+++ src/components/body.js
@@ -0,0 +1,17 @@
+import react from "react";
+
+let body = {()=>
+
+return(
+<div>
+<div className="cuerpo">
+
+</div>
+
+</div>
+
+)
+
+
+
+}
--- src/components/header.js
+++ src/components/header.js
@@ -3,18 +3,15 @@ import react from "react";
 function Header() {
     
     return( <div className="header">
-        <h1 className="titulo">Dual Key gaming</h1>
+        <div><img src="./img/a.png" alt="logo" ></img></div>
+        <div className="contenedor-lista">
         <ul className="lista">
-        <li className="items"> <a href="#">componentes</a></li>
+        <li className="items"> <a href="#">Componentes</a></li>
         <li className="items"><a  href="#">Pc completa</a></li>
         <li className="items"><a  href="#"> Perifericos</a></li>
         </ul>
+        </div>
      </div>
-        
-        
-
-
-
-    )
+     )
 }
 export default Header;
