7990702c6b94d0994a6ce9d9debe564bd01b5e28
Santiago Neyra <santineyra97@gmail.com>
2022-02-04 21:29:54 -0300

    a

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,9 @@ import react from "react";
 function Header() {
     
     return( <div className="header">
-        <div><img src="./img/a.png" alt="logo" ></img></div>
+        <div>
+           <h1 className="titulo">Dual Key Gaming</h1>
+         </div>    
         <div className="contenedor-lista">
         <ul className="lista">
         <li className="items"> <a href="#">Componentes</a></li>
