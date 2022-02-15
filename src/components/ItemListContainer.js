f80c2c2ac39ac0a8878b6eaf8dd10eafba0061b8
Santiago Neyra <santineyra97@gmail.com>
2022-02-15 10:41:29 -0300

    a

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -4,15 +4,19 @@ import ItemCount from "./itemCount";
 function ItemListContainer({greeting}) {
 
 const [productos, setProductos]= useState([]);
-
-const promesa= new Promise((resolve, reject)=>{
-setTimeout(()=>{
-
+// CREAMOS UNA PROMESA QUE TIENE DOS FUNCIONES, RESOLVE Y REJECT
+const promesa = new Promise((resolve, reject)=>{
+//CON ESTE SET TIMEOUT TARDAREMOS 2S EN RECIBIR RESPUESTA, SIMULANDO UN SERVIDOR
+    setTimeout(()=>{
+//NOS RETORNA UN ARREGLO
     resolve(Lista)
 }, 2000);
 
 })
+
+//AL OBTENER LOS DATOS DEL SERVIDOR NOSOTROS UTILIZAMOS 
 useEffect(()=>{
+    //LLAMAMOS A PROMESA CUANDO OBTENEMOS EL RESULTADO
     promesa.then(resultado=>{
      setProductos(resultado)
     })
@@ -36,7 +40,7 @@ return(
 
      <div>
          <h2>{elemento.name}</h2>
-         <img src={elemento.ruta} alt="Producto" /> 
+         <img className="img-producto" src={elemento.ruta} alt="Producto" /> 
         <p>{elemento.description}</p>
         <button className="detalle">VER DETALLE +</button>
         <h4>Precio contado: ${elemento.price}</h4>
